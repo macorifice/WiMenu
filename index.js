5cf5241f4e7d55aed1160cd7fd3e00ebf3ea2f01
Stefano Di Cecco <stefano.dicecco@fitprime.com>
2020-06-09 02:13:42 +0200

    [+] fix deploy heroku-postbuild 7

--- index.js
+++ index.js
@@ -12,13 +12,12 @@ app.use(bodyParser.json());
 require('./routes/menuRoutes')(app);
 
 if (process.env.NODE_ENV === 'production') {
-    app.use(express.static('client/build'));
+    app.use(express.static(path.join(__dirname, 'public')));
   
     const path = require('path');
-    app.get('*', (req,res) => {
-        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
-    })
-  
+    app.get('*', function(req, res) {
+      res.sendFile(path.join(__dirname, 'public', 'index.html'));
+    });
   }
 
 const PORT = 5000;
